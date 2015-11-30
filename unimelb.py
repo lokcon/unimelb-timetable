a1cc30820010af71da112978bfc491ab68eebdf1
Andy Lok <andylokon@gmail.com>
2015-12-01 05:13:02 +0800

    Add CLASS_TYPES and STUDY_PERIODS dictionary to unimelb module

---
+++ unimelb.py
@@ -0,0 +1,44 @@
+CLASS_TYPES = {
+    "C" : "Concert Class",
+    "CL": "Clinical Laboratory",
+    "FW": "Fieldwork",
+    "I" : "Instrument Class",
+    "IP": "Independent Prac",
+    "L" : "Lecture",
+    "LE": "Large Ensemble",
+    "P" : "Practical",
+    "PB": "Problem-based",
+    "PC": "Performance",
+    "PF": "Performance",
+    "R" : "Rehearsal",
+    "S" : "Seminar",
+    "SC": "Screening",
+    "SE": "Small Ensemble",
+    "ST": "Studio",
+    "T" : "Tutorial",
+    "W" : "Workshop"
+}
+
+STUDY_PERIODS = {
+    "JAN": "January",
+    "FEB": "February",
+    "MAR": "March",
+    "APR": "April",
+    "MAY": "May",
+    "JUN": "June",
+    "JUL": "July",
+    "AUG": "August",
+    "SEP": "September",
+    "OCT": "October",
+    "NOV": "November",
+    "DEC": "December",
+    "SM1": "Semester 1",
+    "SM2": "Semester 2",
+    "SUM": "Summer Term",
+    "WIN": "Winter Term",
+    "YRL": "Year Long"
+}
+
+'''A scraper for the timetable of a subject'''
+class Timetale
+    def __init__()
